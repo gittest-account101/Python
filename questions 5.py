254b748aecdf6c720cc2279cd6294d5ce0978443
KaliUsrPurnansh <sharmapurnansh@gmail.com>
2022-04-12 00:06:49 -0700

    update

--- questions 5.py
+++ questions 5.py
@@ -1,4 +1,17 @@
-x=1
-while x==1:
-    print('hi')
-    
+def ques1():
+    def sum(x,y):
+        x+=1
+        y+=1
+        return x , y
+    a=2
+    b=3
+    print(a , b , "\n")
+    a,b=sum(a,b)
+    print(a ,b)
+
+def ques2():
+    #hello
+    x={1:'a', 2:"b" , 3:{31:"ba" , 32:'bb' ,33:{331:'bba'}}}
+    print(x[3][33][331])
+
+ques2()
